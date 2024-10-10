a353debbafee787f367c479baaf686d0a08c6e1f
TonThatRon <tonr84295@gmail.com>
2024-10-10 11:31:45 +0700

    add route view_database

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 # app.py
-from flask import Flask, request, jsonify, render_template, request, redirect, url_for
+from flask import Flask, request, jsonify, render_template, request, redirect, render_template_string
 from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
 
@@ -24,6 +24,36 @@ todos_schema = TodoSchema(many=True)
 def home():
     return render_template('index.html')
 
+@app.route('/view_database')
+def view_database():
+    todos = Todo.query.all()
+    table_html = '''
+    <style>
+        body { font-family: Arial, sans-serif; }
+        table { border-collapse: collapse; width: 100%; }
+        th, td { border: 1px solid #ddd; padding: 8px; }
+        tr:nth-child(even) { background-color: #f2f2f2; }
+        th { padding-top: 12px; padding-bottom: 12px; text-align: left; background-color: #4CAF50; color: white; }
+    </style>
+    <h1>Todo Database</h1>
+    <table>
+        <tr>
+            <th>ID</th>
+            <th>Title</th>
+            <th>Description</th>
+            <th>Completed</th>
+        </tr>
+        {% for todo in todos %}
+        <tr>
+            <td>{{ todo.id }}</td>
+            <td>{{ todo.title }}</td>
+            <td>{{ todo.description }}</td>
+            <td>{{ todo.completed }}</td>
+        </tr>
+        {% endfor %}
+    </table>
+    '''
+    return render_template_string(table_html, todos=todos)
 @app.route('/todos', methods=['GET'])
 def get_todos():
     all_todos = Todo.query.all()
